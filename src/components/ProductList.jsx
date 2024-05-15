90a2960fcdaf1a034619098f5407af443badd4c8
Alfredo Carrasquel <alfreocrdev@gmail.com>
2024-05-14 21:58:09 -0400

    add paginacion de productos

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,20 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import cel from '../img/celulares.webp'
+import cel from '../img/celulares.webp';
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
   useEffect(() => {
-    axios.get('http://localhost:8080/products/')
-      .then(response => {
-        setProducts(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching products:', error);
-      });
-  }, []);
+    fetchProducts();
+  }, [currentPage]);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await axios.get(`http://localhost:8080/products/?page=${currentPage}`);
+      setProducts(response.data.docs);
+      setTotalPages(response.data.totalPages);
+    } catch (error) {
+      console.error('Error fetching products:', error);
+    }
+  };
+
+  const handlePrevPage = () => {
+    setCurrentPage(currentPage - 1);
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
   return (
     <div style={styles.container}>
@@ -30,8 +48,29 @@ const ProductList = () => {
             <p>Precio: $ {(product.price).toLocaleString()}</p>
             <p>Marca: {product.brand}</p>
           </div>
-      ))}
+        ))}
+      </div>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={handlePageChange}
+        onPrevPage={handlePrevPage}
+        onNextPage={handleNextPage}
+      />
     </div>
+  );
+};
+
+const Pagination = ({ currentPage, totalPages, onPageChange, onPrevPage, onNextPage }) => {
+  return (
+    <div style={styles.pagination}>
+      <button onClick={onPrevPage} disabled={currentPage === 1}>
+        Anterior
+      </button>
+      <div>Página {currentPage} de {totalPages}</div>
+      <button onClick={onNextPage} disabled={currentPage === totalPages}>
+        Siguiente
+      </button>
     </div>
   );
 };
@@ -56,11 +95,17 @@ const styles = {
     margin: '1.2rem',
     alignItems: 'flex-end',
     backgroundColor: '#DEDEDE',
-    borderRadius: '16px'
+    borderRadius: '16px',
   },
   cel: {
-    width: '250px'
-  }
-}
+    width: '250px',
+  },
+  pagination: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: '1rem',
+  },
+};
 
 export default ProductList;
